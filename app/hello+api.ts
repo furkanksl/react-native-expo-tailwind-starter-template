71f2b1f0892f69239aa01497100e665f319f53ca
furkanksl <furkankoseoglu65@gmail.com>
2023-09-18 10:03:57 +0300

    Add api route for test ( not available yet )

---
+++ app/hello+api.ts
@@ -0,0 +1,7 @@
+// import { ExpoRequest, ExpoResponse } from "expo-router/server";
+
+// export async function GET(req: ExpoRequest): Promise<ExpoResponse> {
+//     return ExpoResponse.json({ msg: "hello world" });
+// }
+
+//* This feature will be available in the next release of Expo Router (v3.0.0) and Expo (v50.0.0)
