fb26005711197fdb47fa110098680759bcce6d8b
furkanksl <furkankoseoglu65@gmail.com>
2023-09-18 09:15:43 +0300

    Create common constant

---
+++ constants/common.ts
@@ -0,0 +1,9 @@
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
+import { NativeStackNavigationOptions } from "@react-navigation/native-stack";
+
+export const noTabHeader: BottomTabNavigationOptions = {
+    headerShown: false,
+};
+export const noStackHeader: NativeStackNavigationOptions = {
+    headerShown: false,
+};
